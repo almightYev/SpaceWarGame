c6664ffc66cd715aef05d1b72e09bcbe70a80a00
Yevgeni Anischenko <yevkemail@gmail.com>
2021-01-23 20:46:14 +0200

    Add project files.

---
+++ SpaceWarGame/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace SpaceWarGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Shippic = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Shippic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.Shippic);
+            this.panel1.Location = new System.Drawing.Point(-3, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 367);
+            this.panel1.TabIndex = 0;
+            // 
+            // Shippic
+            // 
+            this.Shippic.Image = global::SpaceWarGame.Properties.Resources.ship;
+            this.Shippic.Location = new System.Drawing.Point(3, 154);
+            this.Shippic.Name = "Shippic";
+            this.Shippic.Size = new System.Drawing.Size(90, 90);
+            this.Shippic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Shippic.TabIndex = 0;
+            this.Shippic.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "ShootEmDown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Shippic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Shippic;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SpaceWarGame/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceWarGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveUp()
+        {
+            Point position = Shippic.Location;
+            if(position.Y > 0)
+            {
+                position.Y -= 20;
+                Shippic.ImageLocation = "ship/shipanimation.png";
+            }
+            Shippic.Location = position;
+        }
+
+        private void MoveDown()
+        {
+            Point position = Shippic.Location;
+            if(position.Y < panel1.Height - Shippic.Height)
+            {
+                position.Y += 20;
+                Shippic.ImageLocation = "ship/shipanimation.png";
+            }
+            Shippic.Location = position;
+        }
+
+        private void Fire()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Height = 15;
+            bullet.Width = 25;
+            bullet.ImageLocation = "ship/fire.png";
+            bullet.Location = new Point(Shippic.Location.X + Shippic.Width, Shippic.Location.Y + Shippic.Height / 2);
+            bullet.Name = "Bullet";
+            panel1.Controls.Add(bullet);
+        }
+
+        int totalEnemies = 4;
+        private void creatingEnemies()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(1, totalEnemies + 1);
+            PictureBox enemy = new PictureBox();
+            int location = rnd.Next(0, panel1.Height - enemy.Height);
+            enemy.SizeMode = PictureBoxSizeMode.StretchImage;
+            enemy.ImageLocation = "Aliens/"+ x +".png";
+            enemy.Location = new Point(panel1.Width - enemy.Width, location);
+            enemy.Name = "enemy";
+            panel1.Controls.Add(enemy);
+        }
+
+        private void creatingStars()
+        {
+            Random rnd = new Random();
+            PictureBox star = new PictureBox();
+            star.Width = 10;
+            star.Height = 10;
+            int location = rnd.Next(0, panel1.Height - star.Height);
+            star.ImageLocation = "star/star.png";
+            star.SizeMode = PictureBoxSizeMode.StretchImage;
+            star.Location = new Point(panel1.Width - star.Width, location);
+            star.Name = "star";
+            star.BackColor = Color.Transparent;
+            panel1.Controls.Add(star);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                MoveUp();
+            }
+
+            else if(e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                MoveDown();
+            }
+
+            else if(e.KeyCode == Keys.Space || e.KeyCode == Keys.Enter)
+            {
+                Fire();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down || e.KeyCode == Keys.W)
+            {
+                Shippic.ImageLocation = "ship/ship.png";
+            }
+            else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.S)
+            {
+                Shippic.ImageLocation = "ship/ship.png";
+            }
+        }
+
+        int enemyCounter = 0;
+        int starCounter = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            enemyCounter++;
+            starCounter++;
+
+            for(int i = 0; i < panel1.Controls.Count; i++)
+            {
+                PictureBox pb = ((PictureBox)panel1.Controls[i]);
+                if(panel1.Controls[i].Name == "Bullet")
+                {
+                    if(pb.Location.X + pb.Width > panel1.Width)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X + 5, pb.Location.Y);
+                    }
+                }
+                else if(panel1.Controls[i].Name == "enemy")
+                {
+                    if(pb.Location.X < 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X - 5, pb.Location.Y);
+                    }
+                }
+                else if(panel1.Controls[i].Name == "star")
+                {
+                    if(pb.Location.X < 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X - 5, pb.Location.Y);
+                    }
+                }
+            }
+            if(enemyCounter == 50)
+            {
+                enemyCounter = 0;
+                creatingEnemies();
+            }
+            else if(starCounter == 30)
+            {
+                starCounter = 0;
+                creatingStars();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
